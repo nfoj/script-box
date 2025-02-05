905650295e1b1a396ab21102e0408579a991101f
nfoj <nfoj_@hotmail.com>
2025-02-04 23:09:43 -0300

    [Python] [Script] - Add - internet speed test

--- python/internet_speed_test.py
+++ python/internet_speed_test.py
@@ -28,10 +28,11 @@ colors = {
     'COLOR_RED'  : '\033[1;31m',
     'COLOR_GREEN': '\033[1;32m',
     'NO_COLOR'   : '\033[0m'
-
 }
 
 #!--------------------------------------!#
+
+# import
 
 import sched
 import numpy as np
@@ -39,6 +40,15 @@ import pandas as pd
 import time
 from datetime import datetime
 import speedtest
+
+#!--------------------------------------!#
+
+# init
+
+download = []
+upload = []
+ping = []
+datetime []
 
 st = speedtest.Speedtest()
 scheduler = sched.scheduler()
@@ -63,3 +73,78 @@ def delay():
             break
 
     return delay_time
+
+#!--------------------------------------!#
+
+# test internet speed
+
+def test_internet_speed():
+    global download, upload, ping, datetime
+
+    # Velocity rede
+    test_download = '{:.2f}'.format(st.download() / (10 ** 6))
+    test_upload = '{:.2f}'.format(st.upload() / (10 ** 6))
+    test_ping = st.results.ping
+
+    # Rules
+    test_datetime = time.ctime()
+
+    #
+    download.append(test_download)
+    upload.append(test_upload)
+    ping.append(test_ping)
+    datetime.append(test_datetime)
+
+    #
+    print("Test delay time {delay_time}")
+    
+    scheduler.enter(delay=delay_time, priority=0, action=test_internet_speed)
+
+#!--------------------------------------!#
+
+# 
+
+def df_speedtest():
+    data_speed = {'Download': download, 'Upload': upload, 'Ping': ping}
+    data_speeds = pd.DataFrame(data_speeds, columns=['Download', 'Upload', 'Ping'])
+
+    #
+    date_time = {'Datetime' : datetime, }
+    df_date_time = pd.DataFrame(date_time, columns=['Datetime'])
+
+    #
+    dados_speedtest = pd.concat([df_date_time, data_speeds], axis=1, join='inner')
+    print(dados_speedtest.to_markdown())
+    scheduler.enter(delay=delay_time, priority=1, action=df_speedtest)
+
+
+#!--------------------------------------!#
+
+# save
+
+def save_dataframe():
+    global download, upload, ping, datetime
+
+    data_speeds = {'Download': download, 'Upload': upload, 'Ping': ping}
+    data_speeds = pd.DataFrame(data_speeds, columns=['Download', 'Upload', 'Ping'])
+
+    
+    date_time = {'Datetime': datetime,}
+    df_date_time = pd.DataFrame(date_time, columns=['Datetime'])
+
+    
+    excel_speedtest = pd.concat([df_date_time, data_speeds], axis=1, join='inner')
+    excel_speedtest.to_csv("dados_speedtest.csv", index=False)
+    
+
+#!--------------------------------------!#
+
+# test
+delay_time = delay()
+scheduler.enter(0, 1, test_internet_speed)
+
+try:
+    scheduler.run()
+except KeyboardInterrupt:
+    print("Program interrupted by user. Exiting gracefully...")
+    save_dataframe()  
---
+++ python/python_config_docker.md
@@ -0,0 +1,12 @@
+# Config
+
+```
+
+cd meu_projeto
+apt-get update
+apt-get install python3.12-venv
+python3.12 -m venv .venv
+source .venv/bin/activate
+pip install numpy
+  
+```
